897f341d885e70703250b5b317301777bb69bb22
Damiaan Dufaux <damiaan@dpro.be>
2022-09-09 16:37:46 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "THEOplayerSDK",
+    platforms: [
+        .iOS(.v12),
+        .tvOS(.v12)
+    ],
+    products: [
+        .library(name: "THEOplayerSDK", targets: ["THEOplayerSDK"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "THEOplayerSDK",
+            path: "Frameworks/THEOplayerSDK-basic/THEOplayerSDK.xcframework"
+        )
+    ]
+)
